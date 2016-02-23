e8e4e05b85df5e0719940196dac76c1e06a830c1
florimaros <flori.maros@yahoo.com>
2016-02-23 11:22:11 +0100

    cv page with new function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 "use strict"
 var resume = document.getElementById("resume")
 resume.addEventListener("click", function (event) {
-  event.target.style.display="none";
+  if (event.target.classList.contains("barmi")) {
+    if (event.target.nextElementSibling.style.display==="none") {
+      event.target.nextElementSibling.style.display="block"
+    }
+    else {
+      event.target.nextElementSibling.style.display="none"
+    }
+  }
+
 })
